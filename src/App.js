a1e50bcddb189de7745baa3c2ca38c1156340d7c
Noah A <nanderson815@gmail.com>
2019-03-30 17:09:54 -0400

    linking json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import dogsJSON from './dogs.json';
 
 class App extends Component {
+  state = {
+    dogs: dogsJSON
+  }
+
+
+
   render() {
     return (
       <div className="App">
+        {console.log(this.state)}
+
       </div>
     );
   }
